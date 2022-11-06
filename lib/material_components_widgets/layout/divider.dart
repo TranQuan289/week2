ecfdd855b1a14fed6a1b7853b7b5579312c44d64
TranQuan289 <anhquan28092001@gmail.com>
2022-11-06 16:22:04 +0700

    week2_cn

---
+++ lib/material_components_widgets/layout/divider.dart
@@ -0,0 +1,72 @@
+///Diver
+///color:màu của diver
+///endIndent: không gian trống đến cạnh phải
+///height: chiều cao của Diver
+///indent: không gian trống đến cạnh phải
+///thickness: độ dài của diver
+
+import 'package:flutter/material.dart';
+
+class DividerApp extends StatelessWidget {
+  const DividerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Divider Sample')),
+        body: const DividerExample(),
+      ),
+    );
+  }
+}
+
+class DividerExample extends StatelessWidget {
+  const DividerExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+              child: const Center(
+                child: Text('Above'),
+              ),
+            ),
+          ),
+          const Divider(
+            height: 20,
+            thickness: 5,
+            indent: 20,
+            endIndent: 0,
+            color: Colors.black,
+          ),
+          // Subheader example from Material spec.
+          // https://material.io/components/dividers#types
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            child: Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(
+                'Subheader',
+                style: Theme.of(context).textTheme.bodySmall,
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primary,
+              child: const Center(
+                child: Text('Below'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
